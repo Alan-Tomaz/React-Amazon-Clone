bdf79fefc0131e17f7a2e64ffc40f680b61e704b
Alan <alan4tomaz8@gmail.com>
2023-11-19 00:41:34 -0300

    Commit 5 - 19/11/2023

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,17 @@ import Checkout from './components/Checkout';
 import Header from './components/Header';
 import Home from './components/Home';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from './components/Login';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Header />
 
         <Routes>
-          <Route path='/checkout' element={[<Checkout />]} />
-          <Route path='/' element={[<Home />]} />
+          <Route path='/checkout' element={[<Header />, <Checkout />]} />
+          <Route path='/login' element={[<Login />]} />
+          <Route path='/' element={[<Header />, <Home />]} />
         </Routes>
       </div>
     </Router >
